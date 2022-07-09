d2ce2c767d62237a2acaaff3311eb5ecd55275d8
enidev911 <enidev911@gmail.com>
2022-07-09 03:51:34 -0400

    up-css

--- src/public/js/functions.js
+++ src/public/js/functions.js
@@ -1,5 +1,6 @@
 const module_1 = document.getElementById("module-1");
 const module_2 = document.getElementById("module-2");
+const module_3 = document.getElementById("module-3");
 
 /**
  * 
@@ -10,9 +11,16 @@ function module_change(module) {
   if (this.module === 1) {
     module_1.classList.remove("collapse");
     module_2.classList.add("collapse");
+    module_3.classList.add("collapse");
   }
   else if (this.module === 2) {
     module_1.classList.add("collapse");
+    module_3.classList.add("collapse");
     module_2.classList.remove("collapse");
+  }
+  else if (this.module === 3){
+    module_1.classList.add("collapse");
+    module_2.classList.add("collapse");
+    module_3.classList.remove("collapse");
   }
 }
