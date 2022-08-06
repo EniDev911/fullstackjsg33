416b7f73f7c3938e64940fe6c3f4aab55295c509
EniDev911 <enidev911@gmail.com>
2022-08-06 01:10:51 -0500

    up-global

---
+++ src/public/js/data.js
@@ -0,0 +1,106 @@
+export const projects = [{
+    title: "Desafío 1: Curriculum Vitae",
+    image: "src/public/images/png/curriculum.png",
+    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-1/2-html/cv2",
+    url_demo: "src/chanllenges/module-1/2-html/cv2/index.html",
+    url_zip: "src/public/compressed/cv2.zip",
+    color: "light",
+    background: "secondary",
+    module: 1,
+    active: true
+}, {
+    title: "Desafío 2: Landing Page",
+    image: "src/public/images/png/landing-courses.png",
+    url_code: "https://github.com/EniDev911/fullstackjsg33/blob/main/src/chanllenges/module-1/3-css/landing-page/courses/",
+    url_demo: "src/chanllenges/module-1/2-html/cv2/index.html",
+    url_zip: "src/public/compressed/courses.zip",
+    color: "primary",
+    background: "dark",
+    module: 1
+},
+{
+    title: "Desafío 3: Iguana Page",
+    image: "src/public/images/png/iguana-page.png",
+    url_code: "https://github.com/EniDev911/fullstackjsg33/blob/main/src/chanllenges/module-1/4-positions/iguana-page/",
+    url_demo: "src/chanllenges/module-1/4-positions/iguana-page/",
+    url_zip: "src/public/compressed/iguana-page.zip",
+    color: "success",
+    background: "dark",
+    module: 1
+},
+{
+    title: "Desafío 4: Bootstrap Cuppons",
+    image: "src/public/images/png/bootstrap_cuppons.png",
+    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-1/5-bootstrap/cuppons",
+    url_demo: "src/chanllenges/module-1/5-bootstrap/cuppons/",
+    url_zip: "src/public/compressed/cuppons.zip",
+    color: "info",
+    background: "success",
+    module: 1
+},
+{
+    title: "Desafío Final: Portafolio",
+    image: "src/public/images/png/portfolio-mobile.png",
+    url_code: "https://github.com/EniDev911/EniDev911.github.io",
+    url_demo: "https://enidev911.github.io/",
+    color: "danger",
+    background: "dark",
+    module: 1
+},
+{
+    title: "Desafío 1: Chat Flexbox",
+    image: "src/public/images/png/chat-nertworking.png",
+    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-2/1-flexbox/chat_networking",
+    url_demo: "src/chanllenges/module-2/1-flexbox/chat_networking/index.html",
+    color: "secondary",
+    background: "light",
+    active: true,
+    module: 2
+},
+{
+    title: "Desafío 2: Grid Ecommerce",
+    image: "src/public/images/png/grid-ecommerce.png",
+    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-2/1-flexbox/chat_networking",
+    url_demo: "src/chanllenges/module-2/1-flexbox/chat_networking/",
+    color: "info",
+    background: "light",
+    module: 2
+},
+{
+    title: "Desafío 3: Media Queries Electric Store",
+    image: "src/public/images/png/electric-store.png",
+    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-2/3-media_queries/electric_cars",
+    url_demo: "src/chanllenges/module-2/3-media_queries/electric_cars/",
+    color: "light",
+    background: "info",
+    module: 2
+},
+{
+    title: "Desafío 4: Sass Cards",
+    image: "src/public/images/png/sass-challenge-a.png",
+    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-2/4-sass/bs-card-sass",
+    url_demo: "src/chanllenges/module-2/4-sass/bs-card-sass/",
+    color: "light",
+    background: "secondary",
+    module: 2
+},
+{
+    title: "Desafío 1: Calculando el total",
+    image: "src/public/images/png/js-challenge-1.png",
+    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-3/card-display-total/",
+    url_demo: "src/chanllenges/module-3/card-display-total/",
+    color: "light",
+    background: "secondary",
+    module: 3,
+    active: true
+},
+{
+    title: "Desafío 2: Condiciones en JS",
+    image: "src/public/images/png/js-challenge-2.png",
+    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-3/conditions",
+    url_demo: "src/chanllenges/module-3/conditions/",
+    color: "light",
+    background: "secondary",
+    module: 3,
+},
+]
--- src/public/js/functions.js
+++ src/public/js/functions.js
@@ -1,30 +1,46 @@
+import { projects } from './data.js'
+
+
 const module_1 = document.getElementById("module-1"),
   module_2 = document.getElementById("module-2"),
   module_3 = document.getElementById("module-3"),
-  btns_clone = document.querySelectorAll(".fa-clone");
+  btns_clone = document.querySelectorAll(".fa-clone"),
+  innerCarousel_1 = document.getElementById("inner-carousel-1"),
+  innerCarousel_2 = document.getElementById("inner-carousel-2"),
+  innerCarousel_3 = document.getElementById("inner-carousel-3"),
+  btns_module = document.querySelectorAll(".module");
+
+let template;
 
 /**
  * 
- * @param {string} module - The module to change 
+ * @param {integer} module - The module to change 
  */
-function module_change(module) {
-  this.module = module;
-  if (this.module === 1) {
+
+const module_change = function (module) {
+  if (module === "m-1") {
     module_1.classList.remove("collapse");
     module_2.classList.add("collapse");
     module_3.classList.add("collapse");
+
   }
-  else if (this.module === 2) {
+  else if (module === "m-2") {
     module_1.classList.add("collapse");
     module_3.classList.add("collapse");
     module_2.classList.remove("collapse");
   }
-  else if (this.module === 3) {
+  else if (module == "m-3") {
     module_1.classList.add("collapse");
     module_2.classList.add("collapse");
     module_3.classList.remove("collapse");
   }
 }
+
+btns_module.forEach(btn => {
+  btn.addEventListener("click", () => {
+    module_change(btn.dataset.name)
+  })
+})
 
 function copy_clipboard(text) {
   if (typeof (text) != 'string') {
@@ -58,3 +74,40 @@ btns_clone.forEach(btn => {
     }, 1000)
   })
 })
+
+const fillTemplate = (obj) => {
+  template = ""
+  let active = ""
+  if (obj.active) {
+    active = " active"
+  }
+  template += `
+      <div class="carousel-item${active}">
+        <div class="options bg-${obj.background} bg-gradient rounded-top py-2 text-center">
+          <h2 class="h5 text-${obj.color}">${obj.title}</h2>
+          <a href="${obj.url_demo}" target="_blank" class="btn btn-outline-${obj.color} me-1">Ver demo <i class="fa-solid fa-eye"></i></a>
+          <a href="${obj.url_code}" target="_blank" class="btn btn-outline-${obj.color} me-2">Ver código <i class="fa-solid fa-file-code"></i></a>`
+  if (typeof obj.url_zip != 'undefined') {
+    template += `<a href="${obj.url_zip}" target="_blank" class="btn btn-outline-${obj.color} select-bg" download>Descargar <i class="fa-solid fa-download"></i></a>`
+  }
+  template += `
+        </div>
+        <img src="${obj.image}" alt="${obj.title}" title="${obj.title}" class="d-block w-100">
+      </div>`
+}
+const addProject = () => {
+  projects.forEach(project => {
+    if (project.module === 1) {
+      fillTemplate(project)
+      innerCarousel_1.innerHTML += template
+    } else if (project.module === 2) {
+      fillTemplate(project)
+      innerCarousel_2.innerHTML += template
+    } else if (project.module === 3) {
+      fillTemplate(project)
+      innerCarousel_3.innerHTML += template
+    }
+  })
+}
+
+addProject();
