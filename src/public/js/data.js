0548bf664527ac69913b49f6b537a15b07bc8827
enidev911 <enidev911@gmail.com>
2022-08-10 01:39:14 -0400

    up-url

--- src/public/js/data.js
+++ src/public/js/data.js
@@ -1,137 +1,150 @@
-export const projects = [{
+export const projects = [
+  {
     title: "Desafío 1: Curriculum Vitae",
     image: "src/public/images/png/curriculum.png",
-    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-1/2-html/cv2",
+    url_code:
+      "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-1/2-html/cv2",
     url_demo: "src/chanllenges/module-1/2-html/cv2/index.html",
     url_zip: "src/public/compressed/cv2.zip",
     color: "light",
     background: "secondary",
     module: 1,
-    active: true
-},{
+    active: true,
+  },
+  {
     title: "Desafío 2: Landing Page",
     image: "src/public/images/png/landing-courses.png",
-    url_code: "https://github.com/EniDev911/fullstackjsg33/blob/main/src/chanllenges/module-1/3-css/landing-page/courses/",
-    url_demo: "src/chanllenges/module-1/3-css/landing-page",
+    url_code:
+      "https://github.com/EniDev911/fullstackjsg33/blob/main/src/chanllenges/module-1/3-css/landing-page/courses/",
+    url_demo: "src/chanllenges/module-1/3-css/landing-page/courses/",
     url_zip: "src/public/compressed/courses.zip",
     color: "primary",
     background: "dark",
-    module: 1
-},
-{
+    module: 1,
+  },
+  {
     title: "Desafío 3: Iguana Page",
     image: "src/public/images/png/iguana-page.png",
-    url_code: "https://github.com/EniDev911/fullstackjsg33/blob/main/src/chanllenges/module-1/4-positions/iguana-page/",
+    url_code:
+      "https://github.com/EniDev911/fullstackjsg33/blob/main/src/chanllenges/module-1/4-positions/iguana-page/",
     url_demo: "src/chanllenges/module-1/4-positions/iguana-page/",
     url_zip: "src/public/compressed/iguana-page.zip",
     color: "success",
     background: "dark",
-    module: 1
-},
-{
+    module: 1,
+  },
+  {
     // BOOTSTRAP CUPPON
     title: "Desafío 4: Bootstrap Cuppons",
     image: "src/public/images/png/bootstrap_cuppons.png",
-    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-1/5-bootstrap/cuppons",
+    url_code:
+      "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-1/5-bootstrap/cuppons",
     url_demo: "src/chanllenges/module-1/5-bootstrap/cuppons/",
     url_zip: "src/public/compressed/cuppons.zip",
     color: "info",
     background: "success",
-    module: 1
-},
-{
+    module: 1,
+  },
+  {
     title: "Desafío Final: Portafolio",
     image: "src/public/images/png/portfolio-mobile.png",
     url_code: "https://github.com/EniDev911/EniDev911.github.io",
     url_demo: "https://enidev911.github.io/",
     color: "danger",
     background: "dark",
-    module: 1
-},
-{
+    module: 1,
+  },
+  {
     title: "Desafío 1: Chat Flexbox",
     image: "src/public/images/png/chat-nertworking.png",
-    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-2/1-flexbox/chat_networking",
+    url_code:
+      "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-2/1-flexbox/chat_networking",
     url_demo: "src/chanllenges/module-2/1-flexbox/chat_networking/index.html",
     color: "secondary",
     background: "light",
     active: true,
-    module: 2
-},
-{
-    // GRID 
+    module: 2,
+  },
+  {
+    // GRID
     title: "Desafío 2: Grid Ecommerce",
     image: "src/public/images/png/grid-ecommerce.png",
-    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-2/2-grid/challenge_store",
+    url_code:
+      "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-2/2-grid/challenge_store",
     url_demo: "src/chanllenges/module-2/2-grid/challenge_store",
     color: "info",
     background: "light",
-    module: 2
-},
-{
+    module: 2,
+  },
+  {
     title: "Desafío 3: Media Queries Electric Store",
     image: "src/public/images/png/electric-store.png",
-    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-2/3-media_queries/electric_cars",
+    url_code:
+      "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-2/3-media_queries/electric_cars",
     url_demo: "src/chanllenges/module-2/3-media_queries/electric_cars/",
     color: "light",
     background: "info",
-    module: 2
-},
-{
+    module: 2,
+  },
+  {
     title: "Desafío 4: Sass Cards",
     image: "src/public/images/png/sass-challenge-a.png",
-    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-2/4-sass/bs-card-sass",
+    url_code:
+      "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-2/4-sass/bs-card-sass",
     url_demo: "src/chanllenges/module-2/4-sass/bs-card-sass/",
     color: "light",
     background: "secondary",
-    module: 2
-},
-{
+    module: 2,
+  },
+  {
     title: "Desafío 1: Calculando el total",
     image: "src/public/images/png/js-challenge-1.png",
-    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-3/card-display-total/",
+    url_code:
+      "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-3/card-display-total/",
     url_demo: "src/chanllenges/module-3/card-display-total/",
     color: "light",
     background: "secondary",
     module: 3,
-    active: true
-},
-{
+    active: true,
+  },
+  {
     title: "Desafío 2: Condiciones en JS",
     image: "src/public/images/png/js-challenge-2.png",
-    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-3/conditions",
+    url_code:
+      "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-3/conditions",
     url_demo: "src/chanllenges/module-3/conditions/part-one-border-toggle.html",
     color: "light",
     background: "secondary",
     module: 3,
-},
-{
+  },
+  {
     title: "Desafío 3: Funciones",
     image: "src/public/images/png/functions.png",
-    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-3/functions",
+    url_code:
+      "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-3/functions",
     url_demo: "src/chanllenges/module-3/functions/actividad_cuadrados",
     color: "light",
     background: "secondary",
     module: 3,
-},
-{
+  },
+  {
     title: "Desafío 4: Inmobiliaria",
     image: "src/public/images/png/inmobiliaria.png",
-    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-3/arrays-todolist",
+    url_code:
+      "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-3/arrays-todolist",
     url_demo: "src/chanllenges/module-3/arrays-objects/",
     color: "light",
     background: "secondary",
     module: 3,
-},
-{
+  },
+  {
     title: "Desafío 5: Todo List",
     image: "src/public/images/png/todo-list-js.png",
-    url_code: "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-3/arrays-todolist",
+    url_code:
+      "https://github.com/EniDev911/fullstackjsg33/tree/main/src/chanllenges/module-3/arrays-todolist",
     url_demo: "src/chanllenges/module-3/arrays-todolist/",
     color: "light",
     background: "secondary",
     module: 3,
-},
-]
-
-
+  },
+];
