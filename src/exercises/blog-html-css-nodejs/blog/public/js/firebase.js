c0dcf2f6a3ce5b2348bfbf18741f4a5a7ebdfdb3
enidev911 <enidev911@gmail.com>
2022-08-12 19:22:53 -0400

    up

--- src/exercises/blog-html-css-nodejs/blog/public/js/firebase.js
+++ src/exercises/blog-html-css-nodejs/blog/public/js/firebase.js
@@ -1,6 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
-import "https://www.gstatic.com/firebasejs/9.9.2/firebase-firestore.js";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
 
+// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAoHO2YZuwBbOit1cb__dFoiuVRp8xSITw",
   authDomain: "blogging-enidev911.firebaseapp.com",
@@ -10,7 +13,13 @@ const firebaseConfig = {
   appId: "1:509819174397:web:d12b8b3b1d082a5672ed94",
 };
 
-// Initialize Firebase
-const firebase = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-let db = firebase.firestore();
+// // Get a list of cities from your database
+// async function getCities(db) {
+//   const citiesCol = collection(db, "cities");
+//   const citySnapshot = await getDocs(citiesCol);
+//   const cityList = citySnapshot.docs.map((doc) => doc.data());
+//   return cityList;
+// }
