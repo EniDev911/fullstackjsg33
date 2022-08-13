b73f44afe9c06535e44afe8500162f683c6fd81f
enidev911 <enidev911@gmail.com>
2022-08-13 04:08:34 -0400

    up

--- src/exercises/blog-html-css-nodejs/blog/server.js
+++ src/exercises/blog-html-css-nodejs/blog/server.js
@@ -1,44 +1,42 @@
-const express = require('express');
-const path = require('path');
-const fileupload = require('express-fileupload');
+const express = require("express");
+const path = require("path");
+const fileupload = require("express-fileupload");
 
 let initial_path = path.join(__dirname, "public");
 
 const app = express();
 
-app.set('port', process.env.PORT || 4000)
+app.set("port", process.env.PORT || 4000);
 
 app.use(express.static(initial_path));
 app.use(fileupload());
 
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(initial_path, "home.html"));
+});
 
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(initial_path, "home.html"))
-})
-
-app.get('/editor', (req, res) => {
-    res.sendFile(path.join(initial_path, "editor.html"))
-})
+app.get("/editor", (req, res) => {
+  res.sendFile(path.join(initial_path, "editor.html"));
+});
 
 // upload link
-app.post('/upload', (req, res)=>{
-    let file = req.files.image;
-    let date = new Date();
-    // Image name
-    let image_name = date.getDate() + date.getTime() + file.name;
-    // Image upload path
-    let path = 'public/uploads/' + image_name;
-
-    // create upload
-    file.mv(path, (err, result)=>{
-        if(err){
-            throw err;
-        } else{
-            // our image upload path
-            res.json(`uploads/${image_name}`)
-        }
-    })
-})
-app.listen(app.get('port'), () => {
-    console.log(`Server running on port ${app.get('port')}`);
-})
+app.post("/upload", (req, res) => {
+  let file = req.files.image;
+  let date = new Date();
+  // Image name
+  let image_name = date.getDate() + date.getTime() + file.name;
+  // Image upload path
+  let path = "public/uploads/" + image_name;
+  // create upload
+  file.mv(path, (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      // our image upload path
+      res.json(`uploads/${image_name}`);
+    }
+  });
+});
+app.listen(app.get("port"), () => {
+  console.log(`Server running on port ${app.get("port")}`);
+});
