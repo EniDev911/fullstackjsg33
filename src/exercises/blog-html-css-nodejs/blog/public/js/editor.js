8124fde77cf5000b0a6621b3b4f91a8737221bcb
enidev911 <enidev911@gmail.com>
2022-08-18 06:29:11 -0400

    up

--- src/exercises/blog-html-css-nodejs/blog/public/js/editor.js
+++ src/exercises/blog-html-css-nodejs/blog/public/js/editor.js
@@ -63,6 +63,7 @@ publishBtn.addEventListener("click", () => {
       .catch((err) => {
         console.error(err);
       });
+    // mapbox access
   }
 });
 
---
+++ src/exercises/javascript/100-exercises-arrays/1-forEach.js
@@ -0,0 +1,6 @@
+let languages = ["JavaScript", "C#", "PHP", "Java", "Python"];
+
+languages.forEach((valor, index, arr) => {
+  console.log(`${valor} se encuentra en el indice ${index} dentro del arreglo [${arr}]
+  `);
+});
