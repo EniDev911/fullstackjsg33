2ca43202a516bd20466f121150010f619d8b3168
enidev911 <enidev911@gmail.com>
2023-02-10 13:55:33 -0300

    up

--- src/guides/components/buttons/GuideButtonTop.js
+++ src/guides/components/buttons/GuideButtonTop.js
@@ -1,31 +1,31 @@
 const template = document.createElement('template');
 template.innerHTML = /*html*/`
-   <style>
-   		a {
-   		 	display: inline-block;
-   		 	text-decoration: none;
-   		 	margin: 10px 0;
-   		}
-   		img {
-   			border: 2px solid black;
-   		}
-   </style>
-   <a href="#top" role="button">
-   	 <img alt="button img" src="">
-   </a>
+<style>
+a {
+  display: inline-block;
+  text-decoration: none;
+  margin: 10px 0;
+}
+img {
+  border: 2px solid black;
+}
+</style>
+<a href="#top" role="button">
+  <img alt="button img" src="">
+</a>
 `
 
 class GuideButtonTop extends HTMLElement {
-	color=this.getAttribute('data-color') ?? 'orange';
-	logo=this.getAttribute('data-logo') ?? 'html5'
+	color = this.getAttribute('data-color') ?? 'orange';
+	logo = this.getAttribute('data-logo') ?? 'html5'
 	logoColor = '%23FAC173'
 	badgeUrl = `https://img.shields.io/badge/regresar%20contenido%20principal-%E2%86%A9-${this.color}?style=for-the-badge&amp;logo=${this.logo}&amp;logoColor=${this.logoColor}`
-	constructor(){
+	constructor() {
 		super()
 	}
 
-	connectedCallback(){
-		const shadow = this.attachShadow({mode: "open"})
+	connectedCallback() {
+		const shadow = this.attachShadow({ mode: "open" })
 		console.log(this.getAttribute('data-href'))
 		this.shadowRoot.appendChild(template.content.cloneNode(true));
 		shadow.querySelector('a').setAttribute('href', this.getAttribute('data-href'))
--- src/guides/components/widgets/DetailWidget.js
+++ src/guides/components/widgets/DetailWidget.js
@@ -1,5 +1,5 @@
-const template = document.createElement('template');
-template.innerHTML = `
+const template = document.createElement("template");
+template.innerHTML = /*html*/ `
 <style>
   details {
     border: 2px solid #000;
@@ -12,34 +12,26 @@ template.innerHTML = `
     margin: -0.5em -0.5em 0;
     padding: 0.5em;
     background: linear-gradient(#f26626 50%, #ed704b 0);
-  	cursor: pointer;
     color: #fff;
-  	transition: margin .2s ease-in;
-   }
-  details[open] {
-    padding: 0.5em;
-   }
-
-  details[open] summary {
-  	margin-bottom: 10px;
   }
 </style>
-   <details>
-      <summary>Introducción a HTML5</summary>
-      <slot>desconocido</slot> 
-   </details>
-`
+<details>
+  <summary></summary>
+  <slot>void</slot> 
+</details>
+`;
 
 class DetailWidget extends HTMLElement {
-	constructor(){
-		super();
-	}
-	title=this.getAttribute('data-title') ?? 'detalle';
-	connectedCallback(){
-		const shadow = this.attachShadow({mode: "open"})
-		this.shadowRoot.appendChild(template.content.cloneNode(true));
-		shadow.querySelector('summary').textContent =  this.title
-	}
+  constructor() {
+    super();
+  }
+  title = this.getAttribute("data-title") ?? "detalle";
+  connectedCallback() {
+    const shadow = this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    shadow.querySelector("summary").textContent = this.title;
+    shadow.querySelector("details").setAttribute("open", "true");
+  }
 }
 
-customElements.define('enidev-details', DetailWidget)
+customElements.define("enidev-details", DetailWidget);
