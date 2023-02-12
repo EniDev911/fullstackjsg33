896206d0da94a41c0ac1024c67b98a94408283ba
enidev911 <enidev911@gmail.com>
2023-02-12 03:35:02 -0300

    up

--- src/exercises/javascript/100-exercises-arrays/1-forEach.js
+++
@@ -1,6 +0,0 @@
-let languages = ["JavaScript", "C#", "PHP", "Java", "Python"];
-
-languages.forEach((valor, index, arr) => {
-  console.log(`${valor} se encuentra en el indice ${index} dentro del arreglo [${arr}]
-  `);
-});
--- src/guides/components/widgets/DetailWidget.js
+++ src/guides/components/widgets/DetailWidget.js
@@ -14,7 +14,9 @@ template.innerHTML = /*html*/ `
     background: linear-gradient(#f26626 50%, #ed704b 0);
     color: #fff;  
   }
-
+  ::host(*) { 
+    border: 1px solid red;
+  } 
 </style>
 <details>
   <summary></summary>
@@ -34,6 +36,7 @@ class DetailWidget extends HTMLElement {
     summary.textContent = this.title;
     if (this.getAttribute("data-lang") === "postgresql") {
       summary.style.background = "linear-gradient(#31648c 50%, #11446c 0)"
+      shadow.querySelector("details").style.border = "1px solid #ccc"
     }
 
     shadow.querySelector("details").setAttribute("open", "true");
