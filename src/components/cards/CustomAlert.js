84bd008505755b4483f80f5e139808418bc24a55
enidev911 <enidev911@gmail.com>
2023-03-02 01:02:12 -0300

    up

--- src/components/cards/CustomAlert.js
+++ src/components/cards/CustomAlert.js
@@ -12,7 +12,21 @@ template.innerHTML = /*html*/`
     position: relative;
   }
   ::slotted(p){
-    color: var(--paragraph);
+    color: var(--paragraphs);
+  }
+  ::slotted(p):before {
+    content: url('data:image/svg+xml;utf8,<svg width=\"46\" height=\"46\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\" stroke=\"%23cccccc\" stroke-width=\"2\" fill=\"transparent\"><path d=\"M12,17 L12,19 M12,10 L12,16 M12,3 L2,22 L22,22 L12,3 Z\"/></svg>');
+    position: absolute;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    width: 66px;
+    height: 66px;
+    border-radius: 50%;
+    border: 1px solid #ccc;
+    background-color: var(--main-background);
+    right: -40px;
+    bottom: -60px;
   }
 </style>
 <blockquote>
